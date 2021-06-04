fe74d2ac41e4102f13776ee22a407ad19b053178
David Santamaria <davidsantamaria1245@gmail.com>
2021-06-03 23:57:15 -0400

    Upload files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,15 +7,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
-        System.out.print("What is your age?");
-        int age = in.nextInt();
-        String output;
-        if (age < 16) {
-            output = "You are not old enough to legally drive.";
+        System.out.print("What is the password?");
+        String input = in.nextLine();
+        if (input.equals("abc$123")) {
+            System.out.println("Welcome!");
         } else {
-            output = "You are old enough to legally drive.";
+            System.out.println("I don't know you.");
         }
-        System.out.print(output);
     }
 }
